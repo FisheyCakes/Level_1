582d4492a966cb39e9e2cfb4920a1aba2f6cc7d6
tallyhawley <league@WTS5.attlocal.net>
2017-04-17 18:27:15 -0700

    Day.Roomba

--- src/MyGUI.java
+++ src/MyGUI.java
@@ -1,14 +1,23 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MyGUI {
 	JFrame window;
 JPanel sand;
+JButton ducks;
 	void setup() {
 		window = new JFrame();
 		window.setVisible(true);
 		window.setSize(1000, 1000);
 		sand = new JPanel();
 		window.add(sand);
+		ducks=new JButton("Become Button");
+		sand.add(ducks);
+		window.setSize(500,  500);
+		ducks.addActionListener((stewart)->{
+			JOptionPane.showMessageDialog(null, "Cake");
+		});
 	}
 }
